e5f5d0193aeb409abcc0b3fe3e1ca5cd385b27e7
softmilkpc <70849324+softmilkpc@users.noreply.github.com>
2021-03-07 15:52:00 +0700

    Update config.py

--- bot_poc/config.py
+++ bot_poc/config.py
@@ -1,11 +1,11 @@
 # coding: utf-8
 # 設置 App.config
-SECRET_KEY              = 'b07481bjava7704sd25sd27d53000e6b1daI52f47'
+SECRET_KEY              = 'b8e0fa4025d5d5cf0e5a2b791517654f'
 SESSION_PROTECTION      = 'strong'
 JSON_AS_ASCII           = False
 #CSRF_ENABLED           = True
 TEMPLATES_AUTO_RELOAD   = True
 
 # 設置 bot_poc.config
-BOT_API_KEY             = "1234567890:ABCDEFGHIJKKKKKKKKKK"
+BOT_API_KEY             = "1302043995:AAH73stA1Qzbtabyb4DHigjlUI3qwNE7Hx0"
 BOT_HOOK_URL            = "https://Domainnnnnnnnnnnnn/hook"
